4dd22308911e5a920a5e92791343cb7e0d7df9af
gegeweww <fritzgerald.arsyi@gmail.com>
2026-05-28 14:21:47 +0700

    Percobaan pertama

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
 import streamlit as st
 
-st.title("Hantaran Mutiara")
+# ================= LOGIN =================
+def login():
+    if "logged_in" not in st.session_state:
+        st.session_state.logged_in = False
+
+    if st.session_state.logged_in:
+        return True
+
+    st.title("Login")
+
+    password = st.text_input(
+        "Password",
+        type="password"
+    )
+
+    if st.button("Masuk"):
+        if password == st.secrets["PASSWORD"]:
+            st.session_state.logged_in = True
+            st.rerun()
+        else:
+            st.error("Password salah")
+
+    return False
+
+
+# ================= STOP JIKA BELUM LOGIN =================
+if not login():
+    st.stop()
+
+
+# ================= APP UTAMA =================
+st.sidebar.title("Hantaran Mutiara")
+st.write("Aplikasi jalan di sini")
